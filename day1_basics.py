676fe9c36b4e5f53a49b9ee7cd9cdc11c50ccc8b
ThamalKarunarathna <thamalkarunarathna7@gmail.com>
2025-12-11 09:57:29 +0530

    Day 1 and Day 2 Python exercises

---
+++ day1_basics.py
@@ -0,0 +1,31 @@
+name = "Thamal"
+age = 27
+city = "Colombo"
+goal = "Become an AI Engineer"
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Goal:", goal)
+print("Hello, my name is", name, "and I am", age, "years old.")
+print("I live in", city, "and my goal is to", goal + ".")
+
+a = 8
+b = 3
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Remainder:", a % b)
+print("Exponentiation:", a ** b)
+print("Floor Division:", a // b)
+
+
+
+first = int(input("Enter first number: "))
+second = int(input("Enter second number: "))
+
+total = first + second
+
+print("The total is:", total)
